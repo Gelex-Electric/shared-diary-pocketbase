57d096558843b4b37de9b5bcf9287685e27ce055
GETC <165369748+Gelex-Electric@users.noreply.github.com>
2026-03-24 12:02:43 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,11 @@
 set -e
 
 echo "🚀 Khởi động PocketBase..."
-./pocketbase serve --http=0.0.0.0:8090 --dir=/app/pb_data > /dev/stdout 2>&1 &
+./pocketbase serve \
+  --http=0.0.0.0:8090 \
+  --dir=/app/pb_data \
+  --publicUrl=https://getc.up.railway.app/pb \
+  > /dev/stdout 2>&1 &
 
 sleep 5
 
