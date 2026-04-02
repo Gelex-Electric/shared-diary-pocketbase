776dfacdbf851832ae9cbf39d75ee64d3bb9e169
GETC <thang.nm165570@gmail.com>
2026-04-02 16:50:12 +0700

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -28,3 +28,71 @@ export interface ElectricShift {
   created: string;
   updated: string;
 }
+
+export interface Customer {
+  id: string;
+  Name: string;
+  MKH: string;
+  area: string;
+  created: string;
+  updated: string;
+}
+
+export interface Meter {
+  id: string;
+  MeterNo: string;
+  HSN: string;
+  Type: string;
+  CreatedHES: string;
+  Line: string;
+  Customer: string; // ID of Customer
+  area: string;
+  Activate: boolean;
+  created: string;
+  updated: string;
+  expand?: {
+    Customer?: Customer;
+  };
+}
+
+export interface AccountHes {
+  id: string;
+  Account: string;
+  Password: string;
+  Token: string;
+  area: string;
+  HesID: number;
+  created: string;
+  updated: string;
+}
+
+export interface HesReading {
+  id: string;
+  meterNo: string;
+  date: string;
+  pg: string;
+  bt: string;
+  cd: string;
+  td: string;
+  vc: string;
+  area: string;
+  created: string;
+  updated: string;
+}
+
+export interface HesItem {
+  METER_NO: string;
+  METER_NAME: string;
+  METER_MODEL_DESC: string;
+  CUSTOMER_CODE: string;
+  CUSTOMER_NAME: string;
+  ADDRESS: string;
+  PHONE: string;
+  EMAIL: string;
+  CREATED: string;
+  LINE_NAME: string;
+  COORDINATE: string | null;
+  LINE_ID: string | null;
+  METER_MODEL_ID: string | null;
+  isDuplicate?: boolean;
+}
