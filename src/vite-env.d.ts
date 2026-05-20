787c27888db8ff7a9f935ef46dbff1efb33b29eb
GETC <thang.nm165570@gmail.com>
2026-05-20 16:15:59 +0700

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="vite/client" />
+
+declare module "*.ttf?url" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.csv?raw" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.csv?url" {
+  const content: string;
+  export default content;
+}
